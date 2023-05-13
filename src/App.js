56c5eabdc918775b1bceb235a43b803e6d05e259
Sangit Sigdel <53471276+SangitSigdel@users.noreply.github.com>
2023-05-13 23:55:59 +0100

    chore: modified jenkins file (#6)
    * chore: modified jenkins file
    
    * chore: removed unnecessary jenkins code
    
    * chore: lets see
    
    * chore: some changes
    
    * set pending status
    
    * chore: removed first step
    
    * chore: changed develop branch
    
    * chore: added app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> lets see world now.
+          Edit <code>src/App.js</code> now it ok.
         </p>
         <a
           className="App-link"
