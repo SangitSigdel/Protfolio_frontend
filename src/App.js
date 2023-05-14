e3cda910474ca8168b30b9721df7bce99cb2d38e
Sangit Sigdel <53471276+SangitSigdel@users.noreply.github.com>
2023-05-14 21:54:47 +0100

    Testing removing workspace (#8)
    * chore: failing test
    
    * chore: testing commit push
    
    * chore: revoked app js
    
    * chore: added new path of modified jenkins web server private key
    
    * chore: fix secrete key path
    
    * chore: fix new secrete key path

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> now it ok.
+          Edit <code>src/App.js</code> Hello world from me.
         </p>
         <a
           className="App-link"
